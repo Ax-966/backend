d1f3247798c208a61978718cf3bcd6b6325ca8f5
Axel Agustin Appella <argaxelagustin@gmail.com>
2024-12-31 15:45:16 -0300

    setup

---
+++ index.js
@@ -0,0 +1,28 @@
+import  express  from "express";
+import { configDotenv } from "dotenv";
+import dbConn from "./app/config/db.js";
+import cors from "cors";
+
+
+configDotenv();
+const {
+    PORT = 3000,
+    URI  = "mongodb://localhost:27017/chat-bot"
+} = process.env;
+
+dbConn(URI);
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:4500", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+};
+
+app.use(cors(corsOptions)); 
+
+app.use(express.static('client'));
+
+
+
+app.listen(PORT,  () => console.log(` service running on port ${PORT}`))
